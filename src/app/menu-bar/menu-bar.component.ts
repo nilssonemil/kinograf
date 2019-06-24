6b7f5b5caec4e09810dae6498b64cd5c9dcfb7f4
Emil Nilsson <emil.nilsson@protonmail.com>
2019-06-24 18:08:54 +0200

    Enable searching from MenuBar

--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user/user.service';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-bar',
@@ -8,13 +10,23 @@ import { UserService } from '../user/user.service';
 })
 export class MenuBarComponent implements OnInit {
 
-  constructor(private userService: UserService) { }
+  private searchInput = new FormControl()
+
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) { }
 
   private isLoggedIn: boolean = false
 
   ngOnInit() {
     // TODO: Find out a way to get this to update when user's state is updated
     this.isLoggedIn = this.userService.getUser() != null
+  }
+
+  search() {
+    this.router.navigate(['/search', this.searchInput.value])
+    this.searchInput.setValue("")
   }
 
 }
