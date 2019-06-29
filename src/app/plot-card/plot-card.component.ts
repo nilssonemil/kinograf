e3c619b91b7973bf46f3e7dba3565bbda292cc9a
Emil Nilsson <emil.nilsson@protonmail.com>
2019-06-29 11:11:11 +0200

    Add basic functionality for hiding long plots

--- src/app/plot-card/plot-card.component.ts
+++ src/app/plot-card/plot-card.component.ts
@@ -8,10 +8,18 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PlotCardComponent implements OnInit {
 
   @Input() plot: string
+  @Input() characterLimit: number = 200
+
+  show: boolean = false
 
   constructor() { }
 
   ngOnInit() {
+    // FIXME: This is not really the limit used, but sets the default
+    // value for when the plot should be limited.
+    // TODO: ALso hide the show more/less button if the plot is shorter
+    // than the limit.
+    this.show = this.plot.length < this.characterLimit
   }
 
 }
