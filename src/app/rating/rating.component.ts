36129411f61ae7d6f9ec0ac2afbd33cfc9f90760
Emil Nilsson <emil.nilsson@protonmail.com>
2019-06-29 11:46:57 +0200

    Add rating icons

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -12,8 +12,17 @@ export class RatingComponent implements OnInit {
   @Input() rating: Rating
   @Input() imdbID?: string
 
+  iconPath: string
+
+  icons = new Map([
+    ['Internet Movie Database', 'assets/imdb-icon.svg.png'],
+    ['Rotten Tomatoes', 'assets/rotten-icon.svg.png'],
+    ['Metacritic', 'assets/metacritic-icon.svg.png'],
+  ])
+
   constructor(private imdb: IMDbService) { }
 
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.iconPath = this.icons.get(this.rating.source)
+  }
 }
